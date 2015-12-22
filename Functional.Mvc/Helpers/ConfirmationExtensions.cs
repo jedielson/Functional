ad594ef11b7250b5032820baa3d06f136d421188
jedielson <jedielson.jn@outlook.com>
2015-12-22 09:57:53 -0200

    Criação de helpers

---
+++ Functional.Mvc/Helpers/ConfirmationExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Functional.Mvc.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public static class ConfirmationExtensions
+    {
+        public static MvcHtmlString DeleteConfirmation(
+            this AjaxHelper helper,
+            string linkText,
+            string message,
+            Guid modelId,
+            string action,
+            string controller,
+            IDictionary<string, object> htmlAttributes)
+        {
+            var routeValues = new RouteValueDictionary
+                                  {
+                                      { "ModelId", modelId },
+                                      { "Message", message },
+                                      { "ActionDelete", action },
+                                      { "ControllerDelete", controller }
+                                  };
+            return helper.ModalDialogActionLink(linkText, "Confirmation", "Messages", string.Empty, routeValues, htmlAttributes);
+        }
+    }
+}
---
+++ Functional.Mvc/ViewModels/ConfirmationMessageViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Functional.Mvc.ViewModels
+{
+    using System;
+
+    public class ConfirmationMessageViewModel
+    {
+        public Guid ModelId { get; set; }
+
+        public string Message { get; set; }
+
+        public string ActionDelete { get; set; }
+
+        public string ControllerDelete { get; set; }
+    }
+}
